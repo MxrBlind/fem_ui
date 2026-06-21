b119f846ad36d17d482f8f13e9e8e7b4ecd9c25b
Marcos <marcos_reyes12@hotmail.com>
2026-06-21 16:41:32 -0600

    chore: initial scaffold (Angular 21 + Material 21)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  production: true,
+  apiBaseUrl: 'https://p01--fem-be--4x6ccl57xnzg.code.run',
+  appName: 'FEM Admin',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8080',
+  appName: 'FEM Admin - Development',
+};
